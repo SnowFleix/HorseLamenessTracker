75725e15c968120fdff8226b03f11fd9a8c77728
SnowFleix <38901046+SnowFleix@users.noreply.github.com>
2020-07-21 23:51:06 +0100

    Added slots for HSV bars on change

--- HorseLamenessTracking/configurationpage.h
+++ HorseLamenessTracking/configurationpage.h
@@ -24,6 +24,12 @@ private slots:
     void btnWhite_Click();
     void btnFinish_Click();
     void btnPause_Click();
+    void h_max_Changed();
+    void h_min_Changed();
+    void s_max_Changed();
+    void s_min_Changed();
+    void v_max_Changed();
+    void v_min_Changed();
 private:
     Ui::ConfigurationPage *ui;
 };
