3e5900b52c26e0e9e52b57138bf1e90afdd73c4f
SnowFleix <38901046+SnowFleix@users.noreply.github.com>
2020-08-03 23:40:45 +0100

    Updated button functions

--- HorseLamenessTracking/trackingpage.cpp
+++ HorseLamenessTracking/trackingpage.cpp
@@ -43,7 +43,7 @@ void TrackingPage::btnStart_Clicked() {
 /// Handles the user clicking the stop button
 ///
 void TrackingPage::btnStop_Clicked() {
-
+    this->close();
 }
 
 /////////////////////////////////////////////////////////////
@@ -59,5 +59,5 @@ void TrackingPage::btnCalibrate_Clicked() {
 /// Handles the user clicking the back button
 ///
 void TrackingPage::btnBack_Clicked() {
-
+    delete ui;
 }
